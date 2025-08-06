1c364bd2f3a1fb2af640ac3270a2e8506928370b
jbjzeehad <ilamta.97@gmail.com>
2025-08-06 16:54:07 +0600

    basic routing in nodejs

---
+++ nodejsCoreModules/routingInNodejs/server.js
@@ -0,0 +1,31 @@
+const http = require("http");
+
+// const server = http.createServer((req, res) => {
+//   console.log(req.url, req.method);
+
+//   res.end("Welcome to ToDo App Server");
+// });
+
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method);
+  //   res.end("Welcome to ToDo App Server");
+
+  if (req.url === "/todos" && req.method === "GET") {
+    res.end("All Todos");
+  } else if (req.url === "/todos/create-todo" && req.method === "POST") {
+    res.end("Todo Created");
+  } else {
+    res.end("Route Not Found");
+  }
+});
+
+server.listen(5000, "127.0.0.1", () => {
+  console.log("Server listening to port 5000");
+});
+
+/**
+ * /todos - GET - All Todo
+ * /todos/creat-todo - POST - Create Todo
+ *
+ *
+ */
