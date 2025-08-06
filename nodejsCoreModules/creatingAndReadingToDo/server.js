954a4f361a1d12c8f0702ded7e567378cdc83923
jbjzeehad <ilamta.97@gmail.com>
2025-08-06 22:27:21 +0600

    creating and reading a todo using postman

---
+++ nodejsCoreModules/creatingAndReadingToDo/server.js
@@ -0,0 +1,66 @@
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
+
+const filePath = path.join(__dirname, "./db/todo.json");
+
+const server = http.createServer((req, res) => {
+  // GET all todos
+
+  if (req.url === "/todos" && req.method === "GET") {
+    const data = fs.readFileSync(filePath, { encoding: "utf-8" });
+    res.writeHead(200, {
+      "content-type": "application/json",
+    });
+    res.end(data);
+  }
+
+  //   POST a ToDo
+  else if (req.url === "/todos/create-todo" && req.method === "POST") {
+    let data = "";
+    req.on("data", (chunk) => {
+      data = data + chunk;
+    });
+
+    req.on("end", () => {
+      console.log(data);
+      //   const todo = JSON.parse(data);
+      const { title, body } = JSON.parse(data);
+
+      //   console.log(todo);
+
+      //   console.log({ title, body });
+
+      const createdAt = new Date().toLocaleString();
+
+      const allTodos = fs.readFileSync(filePath, { encoding: "utf-8" });
+
+      const parseAllTodos = JSON.parse(allTodos);
+
+      parseAllTodos.push({ title, body, createdAt });
+
+      fs.writeFileSync(filePath, JSON.stringify(parseAllTodos, null, 2), {
+        encoding: "utf-8",
+      });
+
+      res.end(JSON.stringify({ title, body, createdAt }, null, 2));
+
+      //   console.log(allTodos);
+    });
+
+    // res.end("Todo Created");
+  } else {
+    res.end("Route Not Found");
+  }
+});
+
+server.listen(5000, "127.0.0.1", () => {
+  console.log("Server listening to port 5000");
+});
+
+/**
+ * /todos - GET - All Todo
+ * /todos/creat-todo - POST - Create Todo
+ *
+ *
+ */
